2868b4b35e101c4cfd55a292a51a9beefd4ab761
Javier Carrillo <stringparser@gmail.com>
2014-12-10 20:20:40 +0100

    have to find a better way?

--- examples/path.js
+++ examples/path.js
@@ -1,40 +1,11 @@
 'use strict';
 
 var parth = require('../')();
-var use, result;
 
-use = 'object paths';
-result = parth
-  .set('hello.:there(\\w+).:you')
-  .get('hello.awesome.human');
-console.log('\n -- \n', use, '\n -- ');
-console.log(result);
+parth.set('/');
+parth.set('get /');
+parth.set('get /url/:with/data');
+parth.set('get /:page(\\d+)/:with/data');
+parth.set('get page.:model /:page/:url(\\w+)/with.json');
 
-use = 'unix paths or urls';
-result = parth
-  .set('/hello/:there/:you')
-  .get('/hello/awesome/human/?you=matter');
-console.log('\n -- \n', use, '\n -- ');
-console.log(result);
-
-use = 'fallback';
-result = parth
-  .get('/hello/there/you/awesome', { fallback : true });
-console.log('\n -- \n', use, '\n -- ');
-console.log(result);
-
-use = 'sentences';
-result = parth
-  .set('you are an :there(\\w+) :you')
-  .get('you are an awesome human');
-console.log('\n ', use, '\n -- ');
-console.log(result);
-
-use = 'mix';
-result = parth
-  .set(':method(get|put|delete|post) :model.data /hello/:one/:two?something')
-  .get('get page.data /hello/there/awesome.json?page=10');
-console.log('\n -- \n', use, '\n -- ');
-console.log(result);
-console.log();
-console.log(parth.cache);
+console.log('got', parth.get('/'));
--- index.js
+++ index.js
@@ -1,187 +1,121 @@
 'use strict';
 
-var Url = require('url');
-var type = require('utils-type');
-var merge = require('utils-merge');
+var util = require('./lib/util');
+util.boil = require('./lib/boil');
+util.parse = require('./lib/parse');
 
 exports = module.exports = Parth;
 
-function Parth(cache){
+function Parth(){
 
   if( !(this instanceof Parth) ){
-    return new Parth(cache);
+    return new Parth();
   }
 
-  function parth(path, opts){
-    return parth.set(path, opts);
-  }
-  merge(parth, this);
+  this.cache = { paths: [ ], regexp: [ ], masterRE: [ ]  };
+  this.method = { _: {boil:[], parse:[]}, boil:{}, parse:{} };
 
-  parth.cache = type(cache).plainObject || { };
-  parth.cache.paths = type(parth.cache.paths).array || [ ];
-  parth.cache.regexp = type(parth.cache.regexp).array || [ ];
-  parth.cache.masterRE = type(parth.cache.masterRE).array || [ ];
+  // default boilers
+  util.boil._.forEach(function(method){
+    this.boil('#' + method, util.boil[method]);
+  }, this);
 
-  return parth;
+  // default parsers
+  util.parse._.forEach(function(method){
+    this.parse('#' + method, util.parse[method]);
+  }, this);
 }
 
-//----
-// ## tokenize path
-//  > use / and . to tokenize a path to an invariant
-//----
-
-Parth.prototype.tokenize = function(path, opt){
-  var p = type(path).plainObject
-    || { input : (type(path).string || '').trim() };
-  // return early
-  if(!p.input){ return null; } if(p.argv){ return p; }
-
-  opt = type(opt).plainObject || { };
-  opt.sep = type(opt.sep).regexp || /[\/\.]+/g;
-  opt.param = type(opt.group).regexp || /\:(\w+)(\(.+?\))?/g;
-
-  p.path = p.input;
-
-  var url;
-  if((url = p.input.match(/\/\S+/))){
-    url = Url.parse(url[0]);
-    if(url.hash){ p.hash = url.hash; }
-    if(url.query){ p.query = url.search; }
-    p.path = p.input.replace(url.path, url.pathname);
-  }
-
-  p.path = p.path
-    .replace(new RegExp(opt.sep.source+'$'), '').trim();
-
-  // use space as invariant sep token
-  p.argv = p.path
-    .replace(/\S+/g, function($0){
-      var sep = $0.match(/[\/]+/) || $0.match(/[\.]/);
-      $0.replace(opt.param, function($0, label, regexp){
-        p.params = p.params || { };
-        p.params[label] = regexp || '([^\\' +(sep || '/')+ ' ]+)';
-      });
-      if(!sep){ return $0; }  sep = '\\' + sep[0];
-      return $0.replace(new RegExp(sep,'g'), ' ');
-    }).trim().replace(/[ ]+/g, ' ');
-
-  p.depth = p.argv.split(/[ ]/).length;
-
-  // wipe
-  url = null;
-  return p;
-};
-
-//----
-// ## parse path
-//  > parse the path using previous tokens
-//----
-
-Parth.prototype.parse = function(path, opt){
-  var p = this.tokenize(path, (opt = opt || { }));
-  if(!p){ return null; }
-
-  // parse regexp
-  if(!p.regexp){
-
-    opt.flag = type(opt.flag).string || 'i';
-    p.regexp = p.path
-      .replace(/\(.+?\)/, '')
-      .replace(/\S+$/,
-        function($0){ return $0 + ($0.match(opt.sep) || [' '])[0]; })
-      .replace(opt.sep, '\\$&')
-      .replace(/[ ]+/g, '[ ]+').replace(/\+$/,'')
-      .replace(/\:(\w+)/g, function($0, label){ return p.params[label]; });
-
-    p.regexp = new RegExp('^' + p.regexp + '?', opt.flag);
-    return p;
-  }
-
-  // parse params for this.get
-  p.params = { };
-
-  var index = 0;
-  var params = p.input
-    .replace(p.query, '')
-    .match(p.regexp).slice(1);
-
-  p.path.replace(/\:(\w+)/g,
-    function($0, label){
-      return (p.params[label] = params[index++]);
-    });
-
-  return p;
-};
 //
 // ## set a path
 //
-Parth.prototype.set = function(path, opt){
-  var p = this.parse(path, (opt = opt || { }));
-  if(!p){ return this; }
-
-  var index, cache = this.cache;
-  // check + prepare paths and regexes
-  if(cache.regexp.length < p.depth+1){
-    index = cache.regexp.length;
-    while(index < p.depth+1){
-      cache.paths.push([ ]);
-      cache.regexp.push([ ]);
-      index = cache.masterRE.push(null);
-    }
-  }
-
-  // already in cache?
-  if(cache.paths[p.depth].indexOf(p.path) > -1){
-    p = method = index = null; // wipe
-    return this;
-  }
-
-  // prepend for custom regexp or raw, append for default
-  var method = ((/\(.+?\)/).test(p.input) || !(/\:\w+/).test(p.input))
-    ? 'unshift' : 'push';
-  cache.paths[p.depth][method](p.path);
-  cache.regexp[p.depth][method](p.regexp);
-
-  // adjust masterRE
-  cache.masterRE[p.depth] =
-    new RegExp(
-      cache.regexp[p.depth]
-        .map(function(regexp){ return regexp.source; })
-        .join('|'),
-    opt.flag);
-
-  // wipe
-  p = method = index = null;
-  return this;
+Parth.prototype.set = function(path, o){
+  o = util.type(o).plainObject || { };
+  var p = this.boil('#set')(path, o);
+  if(!p){ o = null; return this; }
+  return this.parse('#set')(path, o);
 };
 
 //
 // ## get a previously set path
 //
 
-Parth.prototype.get = function(path, opt){
-  var p = this.tokenize(path, (opt = opt || { }));
-  if(!p.path){ return null; }
+Parth.prototype.get = function(path, o){
+  o = util.type(o).plainObject || { };
+  var p = this.boil('#get')(path, o);
+  if(!p){ o = null; return this; }
+  return this.parse('#get')(path, o);
+};
 
-  var fallback = true;
-  var index = 0, depth = p.depth;
-
-  while(fallback){
-    p.regexp = this.cache.masterRE[depth];
-    fallback = !p.regexp && (this.cache.fallback || opt.fallback);
-    if(!p.regexp || !p.regexp.test(p.input)){
-      if(!fallback){ return null; }
-      depth--; p.fallback = true;
-    }
-    if(depth < 0){ return null; }
+// ## Parth.boil
+// > premise: transform input to an array
+// > have all extra information on opts
+//
+// arguments
+//  - `prop` type `string`
+//  - `boiler` type `function` optional
+//
+// return
+//  - `boiler` function if arguments < 2
+//  - `this` otherwise
+//
+Parth.prototype.boil = function(prop, boiler){
+  if(arguments.length < 2){ return this.method.boil[prop] || util.boil; }
+  if(typeof prop !== 'string'){
+    prop = JSON.stringify(prop);
+    throw new util.Error(
+      ' While setting `'+prop+'` at this.boil(prop[, boiler]):\n' +
+      ' > `prop` should be a string and, if given, `boiler` a function');
   }
+  // add it to the list of boilers
+  if(!this.method.boil[prop]){ this.method._.boil.push(prop); }
 
-  p.regexp = this.cache.regexp[depth];
-  while( !p.regexp[index].test(p.path) ){ index++; }
-  p.regexp = this.cache.regexp[depth][index];
-  p.path = this.cache.paths[depth][index];
+  var self = this;
+  this.method.boil[prop] = function(stems, opts){
+    stems = util.boil(stems, opts);
+    stems = boiler.call(self, stems, opts);
+    if(!stems){ return null; }
+    if(util.type(stems).array){ return stems; }
+    throw new util.Error(
+      ' While boiling `'+prop+'` with boiler(stems, opts):\n'+
+      ' > a boiler should return an array');
+  };
 
-  // wipe
-  fallback = index = depth = null;
-  return this.parse(p, opt);
+  return this;
+};
+
+// ## Parth.parse
+// > premise: transform output to an object
+//
+// arguments
+// `prop` type `string`
+// `parser` type `function` optional
+//
+// return
+//  - `parser` function if arguments < 2
+//  - `this` otherwise
+//
+Parth.prototype.parse = function(prop, parser){
+  if(arguments.length < 2){ return this.method.parse[prop] || util.parse; }
+  if(typeof prop !== 'string'){
+    prop = JSON.stringify(prop);
+    throw new util.Error(
+      ' While setting `'+prop+'` at this.parse(prop[, parser]):\n' +
+      ' > `prop` should be a string and, if given, `parser` a function');
+  }
+  // add it to the list of parsers
+  if(!this.method.parse[prop]){ this.method._.parse.push(prop); }
+
+  var self = this;
+  this.method.parse[prop] = function(/* arguments */){
+    var parsed = parser.apply(self, arguments);
+    if(!parsed){ return null; }
+    if(util.type(parsed).object){ return parsed; }
+    throw new util.Error(
+      ' While parsing `'+prop+'` \n'+
+      ' parser.apply(self, arguments):\n'+
+      ' > a parser should return an object');
+  };
+
+  return this;
 };
