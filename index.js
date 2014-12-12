0e7cea297adf5e7f0d2c3a94719fa45e96c2fde1
Javier Carrillo <stringparser@gmail.com>
2014-12-12 10:28:35 +0100

    url.pathname -> url.path

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function Parth(){
 
 // ## Parth.set
 // > premise: set a string, array path or regexp path
-// > TODO: regexp is not implemented yet
+// > TODO: implement regexp input, not done yet :D
 //
 // arguments
 //  - `path` type `string` or `array`
@@ -53,7 +53,7 @@ Parth.prototype.set = function(path, o){
     }).replace(/[\/\.\?\#]+/g, '\\$&')
       .replace(/\:\w+/g, function($0){ return o.params[$0]; });
 
-  if(o.url && o.url.pathname.length > 1){
+  if(o.url && o.url.path.length > 1){
     o.regexp = o.regexp.replace(/\/\S+/, '$&\\/?');
   }
   o.regexp = o.regexp.replace(/\^\]\+/g, ' ]+');
