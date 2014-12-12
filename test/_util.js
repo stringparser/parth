b1848e7c09bed5ba4b2f25558525c781e1dd3509
Javier Carrillo <stringparser@gmail.com>
2014-12-12 12:02:38 +0100

    test/_util.js ocd

--- test/_util.js
+++ test/_util.js
@@ -5,25 +5,28 @@ var fs = require('fs');
 module.exports = {
   testSuite : function(){
     var testSuite = fs.readdirSync(__dirname);
+
     var exclude = [
       '_main.js',
       '_util.js'
     ];
+
     var testFirst = [
       'args.js',
       'unix-paths.js'
     ];
+
     var last = [
       'url.js',
       'combined.js',
       'params.js'
     ];
 
-
     // use it also to omit _main & _util files
     exclude.concat(testFirst, last).forEach(function(file){
       testSuite.splice(testSuite.indexOf(file), 1);
     });
+
     return testFirst.concat(testSuite, last);
   }
 };
