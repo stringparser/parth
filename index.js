2f61704942da17d4372bb63156197618eeb6cbab
Javier Carrillo <stringparser@gmail.com>
2014-12-15 14:18:22 +0100

    cache props that do not need to be array and other things

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ exports = module.exports = Parth;
 function Parth(){
   if(!(this instanceof Parth)){ return new Parth(); }
   this.cache = {
-    paths: [ ],
-    regexp: [ ],
-    masterRE: {_:[]}
+    paths: Object.create(null),
+    regex: Object.create(null),
+    masterRE: Object.create(null)
   };
+  this.cache.masterRE._ = [ ];
 }
 
 // ## Parth.set
@@ -28,24 +29,22 @@ function Parth(){
 util.paramRE = /(^|\W)\:([^?#.(\/\\ ]+)(\(.+?\))?/g;
 
 Parth.prototype.set = function(path, o){
-  if(!util.boil(path, (o = o || { }))){ return null; }
+  o = o || { }; o.notFound = !util.boil(path, o);
+  if(o.notFound){ return null; }
 
   var cache = this.cache;
   o.found = cache.paths[o.depth]; // already defined
   if(o.found && (o.index = o.found.indexOf(o.path)) > -1){
-    o.regexp = o.found[o.index]; // provide the regexp
+    o.regex = o.found[o.index]; // provide the regexp
     return this;
   }
 
-  if(cache.paths.length < o.depth + 1){
-    o.index = cache.paths.length;
-    while(o.index++ < o.depth + 1){ // prepare cache arrays
-      cache.paths.push([ ]);
-      o.index = cache.regexp.push([ ]);
-    }
+  if(!cache.paths[o.depth]){
+    cache.paths[o.depth] = [ ];
+    cache.regex[o.depth] = [ ];
   }
 
-  o.regexp = '^' + o.path.replace(/\S+/g, function(stem){
+  o.regex = '^' + o.path.replace(/\S+/g, function(stem){
       o.sep = (/\//).test(stem) ? '/#?' : '.';
       return stem.replace(util.paramRE, function($0, $1, $2, $3){
         return $1 + ($3 || '([^' + o.sep + '^]+)');
@@ -59,16 +58,15 @@ Parth.prototype.set = function(path, o){
 
   // update depths
   if(!cache.masterRE[o.depth]){
-    cache.masterRE._.push(o.depth);
+    cache.masterRE.length = cache.masterRE._.push(o.depth);
     cache.masterRE._ = cache.masterRE._.sort();
-    cache.masterRE.depth = cache.regexp.length-1;
   }
 
   cache.paths[o.depth][o.method](o.path);
-  cache.regexp[o.depth][o.method](new RegExp(o.regexp, 'i'));
+  cache.regex[o.depth][o.method](new RegExp(o.regex, 'i'));
 
   cache.masterRE[o.depth] =
-    new RegExp(cache.regexp[o.depth]
+    new RegExp(cache.regex[o.depth]
       .map(function(re){ return re.source; }).join('|')
       .replace(/[\(\)]+/g,''), 'i');
 
@@ -86,27 +84,27 @@ Parth.prototype.set = function(path, o){
 // return `o`
 //
 Parth.prototype.get = function(path, o){
-  o = o || { }; o.notFound = true; // start as not found
-  if(!util.boil(path, o)){ return null; } // path not valid
+  o = o || { };
+  o.notFound = !util.boil(path, o); // start as not found
+  if(o.notFound){ return null; }
 
   o.found = this.cache.masterRE;
-  o.index = true; o.depth = o.found._.slice();
+  o.index = o.depth = o.found.length;
 
   while(o.index){
-    o.index = o.depth.pop();
+    o.index = o.found._[--o.depth];
     if(o.index && o.found[o.index].test(o.path)){
       o.depth = o.index; o.found = o.path; o.index = null;
     }
   }
   if(o.index !== null){ return null; }
 
-  o.index = 0;
-  o.regexp = this.cache.regexp[o.depth];
-  while(!o.regexp[o.index].test(o.path)){ o.index++; }
+  o.index = 0;  o.regex = this.cache.regex[o.depth];
+  while(!o.regex[o.index].test(o.path)){ o.index++; }
 
-  o.regexp = o.regexp[o.index];
+  o.regex = o.regex[o.index];
   o.path = this.cache.paths[o.depth][o.index];
-  o.params = { _: o.found.match(o.regexp).slice(1) };
+  o.params = { _: o.found.match(o.regex).slice(1) };
 
   o.notFound = !(/[ ]+/).test(
     o.found.replace(
