c3078a0d433553a6189f9feee7f6d4a2ca1beb29
Javier Carrillo <stringparser@gmail.com>
2015-03-13 00:53:13 +0100

    update case for perfect match in notFound

--- test/notFound.js
+++ test/notFound.js
@@ -5,14 +5,14 @@ var path, stems, regex, o;
 module.exports = function(Parth){
   var parth = new Parth();
 
-  it('should be an empty string for perfect match', function(){
+  it('should be false for perfect match', function(){
     stems = ':method(get) /hello/:there';
     path = 'get /hello/awesome?query#hash';
 
     regex = parth.set(stems);
     regex.should.not.be.eql(null);
     parth.get(path, (o = { }));
-    o.notFound.should.be.eql('');
+    o.notFound.should.be.eql(false);
   });
 
   it('should have what is left of the path', function(){
