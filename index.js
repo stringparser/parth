d173b88e933002cc84b08837411160ebc0e9a5c1
Javier Carrillo <stringparser@gmail.com>
2015-03-12 21:50:52 +0100

    improvements: - less checks on instantiation - better more consistent docs - fixed bug with paramsRE where /:foo(regex)/:bar(regex) was not correctly taken into account - Now it is possible to escape properly all outside parens :) - Lint everything - fixed bug for root url / where it did not get the optional query - do not give raw path advantadge for regex ordering for now - o.notFound has now the portion from the path that did not match, so in case is an empty string notFound will be falsy

--- index.js
+++ index.js
@@ -6,36 +6,39 @@ util.boil = require('./lib/boil');
 exports = module.exports = Parth;
 
 function Parth(){
-  if(!(this instanceof Parth)){ return new Parth(); }
-  this.store = Object.create(null);
-  this.regex = Object.create(null);
-  this.master = Object.create(null);
-  this.master.length = 0;
+  if(this instanceof Parth){
+    this.store = Object.create(null);
+    this.regex = Object.create(null);
+    this.master = {length: 0};
+    return this;
+  }
+  return new Parth();
 }
 
-// ## Parth.set(path[, opt])
-// > convert a string or array path to a regexp
-// > TODO: implement regexp input
+// ## parth.set(path)
+// > convert a path to a regular expression
+// > TODO: regexp input
 //
 // arguments
-//  - `path` type `string` or `array`
-//  - `opt` type `object` optional holding all extra information
+//  - path, type `string`
 //
-// return `this`
+// throws error if the path type is not supported
+//
+// returns a regular expression for the path
+//
 // --
 // api.public
 // --
 //
 
-util.paramRE = /(^|\W)\:([^()?#\.\/ ]+)(\(+[^ ]*\)+)?/g;
+util.paramRE = /(^|\W)\:([^(?#/.: ]+)(\([^)]*?\)+)?/g;
 
 Parth.prototype.set = function(p){
-  var o = Object.create(null);
-  if(!util.boil(p, o)){ return null; }
+  var o = util.boil(p);
   if(this.store[o.path]){
-    o = this.store[o.path];
-    return o.regex;
-  } // ^ already defined
+    o.match = o.path;
+    return this.store[o.path].regex;
+  }
 
   // # of default and custom regex
   var sep; o.custom = o.default = 0;
@@ -46,8 +49,12 @@ Parth.prototype.set = function(p){
       if($3){ o.custom++; } else { o.default++; }
       return $1 + ($3 || '([^' + sep + '^]+)');
     });
-  }).replace(/[^\/ ]*\/\S+/g, '$&\\/?(?:[^ ])?') // includes /
-    .replace(/\^\]\+/g, ' ]+'); // default params;
+  }).replace(/[^\/ ]*\/\S*/g, '$&/?(?:[^\\/ ]+)?') // includes /
+    .replace(/\^\]\+/g, ' ]+') // default params
+    .replace(/[^()]+(?=\(|$)/g, function($0){ // scape all outside parens
+      return $0.replace(/[\/\.]/g, '\\$&');
+    });
+
 
   // update depths
   o.depth = o.argv.length;
@@ -66,7 +73,7 @@ Parth.prototype.set = function(p){
   // reorder them
   this.regex[o.depth].push(o.regex);
   this.regex[o.depth] = this.regex[o.depth].sort(function(a, b){
-    return ((a.def - a.cust) || -Infinity) - (b.def - b.cust);
+    return (a.def - a.cust) - (b.def - b.cust);
   });
 
   // sum up all learned: void groups and make it one
@@ -82,28 +89,28 @@ Parth.prototype.set = function(p){
 };
 
 
-// ## Parth.get
+// ## parth.get(path[, options])
 // > take a string or array, return the matching path
 //
 // arguments
-//  - `path` type `string` or `array`
-//  - `o` type `object` optional holding all extra information
+//  - path, type `string` or `array`
+//  - options, type `object` optional holding all extra information
 //
-// return `o`
+// returns regex matching path
 // --
 // api.public
 // --
 //
 Parth.prototype.get = function(p, o){
-  o = o || { }; o.notFound = true;
-  if(!util.boil(p, o)){ return null; }
+  o = util.boil(p, o);
   if(this.store[o.path]){
-    util.merge(o, this.store[o.path]);
-    o.match = o.path; return o.regex;
+    return util.merge(o, this.store[o.path]).regex;
   }
 
+  o.notFound = true;
   var found = this.master;
-  var index = this.master.length;
+  var index = (this.store[o.argv.length] || found).length;
+
   while(index > -1){
     if(found[index] && found[index].test(o.path)){
       found = o.path.match(found[index]).slice(1);
@@ -119,12 +126,10 @@ Parth.prototype.get = function(p, o){
 
   index = 0;
   regex.path.replace(util.paramRE, function($0, $1, $2){
-    var p = o.params._[index];
-    var num = Number(p) + 1;
-    o.params[$2] = o.params._[index] = num ? num-1 : p;
+    o.params[$2] = o.params._[index];
     o.params._[index++] = $2;
   });
 
-  o.notFound = !(/[ ]/).test(o.path.replace(o.match, '')[0] || ' ');
+  o.notFound = o.path.replace(o.match, '');
   return regex;
 };
