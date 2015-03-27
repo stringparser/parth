e6fdb4e63ebb735b84c77597506935e7954e7cc1
Javier Carrillo <stringparser@gmail.com>
2015-03-27 18:54:34 +0100

    add test for groups without params

--- test/params.js
+++ test/params.js
@@ -39,4 +39,21 @@ module.exports = function(Parth){
       you: '10'
     });
   });
+
+  it('do not have to contain a label', function(){
+    stems = '(get|post) /hello/:there/:you';
+    path = 'post /hello/awesome/10/?query#hash';
+    parth.add(stems);
+    regex = parth.match(path, (o = { }));
+
+    o.path.should.be.eql(path.replace(/\/\?[^ ]+/, ''));
+    regex.path.should.be.eql(':0'+stems);
+
+    o.params.should.be.eql({
+      '0': 'post',
+      _ : ['0', 'there', 'you'],
+      there: 'awesome',
+      you: '10'
+    });
+  });
 };
